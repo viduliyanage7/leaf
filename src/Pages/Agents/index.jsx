612deb85f29b54a3e9dd0dc85fb0b52ee1ad812c
Mineth Vismitha <minethvismitha@gmail.com>
2024-06-29 14:24:39 +0530

    page heading

--- src/Pages/Agents/index.jsx
+++ src/Pages/Agents/index.jsx
@@ -1,51 +1,88 @@
-import React, { useState } from 'react';
-import { Tabs, Tab, Box } from '@mui/material';
-import Agentsdetails from './agents-details';
-import Agentsnew from './agents-new';
+import React from 'react';
+import { Button, Grid } from '@mui/material';
+import { GoPlus } from "react-icons/go";
+import { Input } from 'antd';
+import { IoSearch } from "react-icons/io5";
+import { Select } from 'antd';
+import { useNavigate } from 'react-router-dom';
 
-const Agentsindex = (props) => {
-    const { setMessage, handleClicksucess, handleClickerr } = props;
+const Agentsindex = () => {
+    const navigate = useNavigate();
 
-    const [tabValue, setTabValue] = useState(0);
-
-    const handleTabChange = (event, newValue) => {
-        setTabValue(newValue);
+    const handleClick = () => {
+        navigate('/New-manure-type');
     };
 
     return (
-        <div><br/>
-             <p style={{ fontSize: '30px', marginTop: '0px',fontWeight:'600' }}>Agents</p>
-    
-            <Tabs  value={tabValue} onChange={handleTabChange} aria-label="Leaf Records Tabs">
-                <Tab label="Agent Details" />
-                <Tab label="Add New Agent" />
-            </Tabs>
-            <TabPanel value={tabValue} index={0}>
-                <Agentsdetails />
-            </TabPanel>
-            <TabPanel value={tabValue} index={1}>
-                <Agentsnew setMessage={setMessage} handleClicksucess={handleClicksucess} handleClickerr={handleClickerr} />
-            </TabPanel>
-        </div>
-    );
-};
-
-function TabPanel(props) {
-    const { children, value, index, ...other } = props;
-
-    return (
-        <div
-            role="tabpanel"
-            hidden={value !== index}
-            id={`tab-${index}`}
-            aria-labelledby={`tab-${index}`}
-            {...other}
-        >
-            {value === index && (
-                <Box sx={{ p: 3 }}>
-                    {children}
-                </Box>
-            )}
+        <div className="bg-gray-100 min-h-screen" style={{ paddingLeft: '20%', paddingRight: '8%'  }}>
+            <div className="bg-green-800 h-18 -m-2"></div>
+            <br />
+            <div className="flex justify-center" >
+                <div className="bg-white mt-12 w-[100%] p-6 rounded-lg" style={{ border: '1px solid #DEDEDE' }}>
+                    <Grid container spacing={6}>
+                        <Grid item xs={4}>
+                            <p className="mt-0">Show&nbsp;&nbsp;
+                                <Select
+                                    showSearch
+                                    defaultValue="10"
+                                    optionFilterProp="label"
+                                    disabled
+                                />&nbsp;&nbsp;
+                                entries
+                            </p>
+                        </Grid>
+                        <Grid item xs={5}>
+                            <Input
+                                placeholder="Search..."
+                                className="h-10"
+                                prefix={<IoSearch className="text-gray-600" size={18} />}
+                            />
+                        </Grid>
+                        <Grid item xs={3}>
+                            <Button
+                                variant="contained"
+                                onClick={handleClick}
+                                className="bg-green-700 rounded-lg h-10 w-30"
+                                style={{ background: '#209F20', borderRadius: '8px', padding: '12px 20px 12px 20px', boxShadow: 'none' }}
+                            >
+                                <GoPlus size={22} />&nbsp;&nbsp;Add New
+                            </Button>
+                        </Grid>
+                    </Grid>
+                </div>
+            </div>
+            <div className="flex justify-center mt-12">
+                <div className="bg-white mt-12 w-[100%] p-6 rounded-lg">
+                    <Grid container spacing={6}>
+                        <Grid item xs={4}>
+                            <p className="mt-0">Show&nbsp;&nbsp;
+                                <Select
+                                    showSearch
+                                    defaultValue="10"
+                                    optionFilterProp="label"
+                                    disabled
+                                />&nbsp;&nbsp;
+                                entries
+                            </p>
+                        </Grid>
+                        <Grid item xs={5}>
+                            <Input
+                                placeholder="Search..."
+                                className="h-10"
+                                prefix={<IoSearch className="text-gray-600" size={18} />}
+                            />
+                        </Grid>
+                        <Grid item xs={3}>
+                            <Button
+                                variant="contained"
+                                className="bg-green-700 rounded-lg h-10"
+                            >
+                                <GoPlus size={22} />&nbsp;&nbsp;Add New
+                            </Button>
+                        </Grid>
+                    </Grid>
+                </div>
+            </div>
         </div>
     );
 }
