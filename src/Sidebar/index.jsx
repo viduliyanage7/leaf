05298a48b9de113f433b12e6e0983609656b2f7c
Mineth Vismitha <minethvismitha@gmail.com>
2024-06-29 11:49:20 +0530

    Changes new

--- src/Sidebar/index.jsx
+++ src/Sidebar/index.jsx
@@ -11,6 +11,7 @@ import { IoIosArrowDown } from "react-icons/io";
 import { AiOutlineDollarCircle } from "react-icons/ai";
 import { FaPersonDigging } from "react-icons/fa6";
 import { MdLogout } from "react-icons/md";
+import {House, Leaf, UsersThree, Handshake, CurrencyCircleDollar, Shovel, CaretDown} from "@phosphor-icons/react";
 
 const Sidebar = () => {
     const [isManureOpen, setIsManureOpen] = useState(false);
@@ -25,17 +26,17 @@ const Sidebar = () => {
 
 
                 <ul>
-                    <li><Link to="/Dashboard" style={{ display: 'flex' }}><GoHome size={22} style={{ color: '#adadad' }} /><p style={{ color: '#adadad', fontSize: '19px', marginLeft: '15px' }}>Dashboard</p></Link></li>
-                    <li><Link to="/leaf-records" style={{ display: 'flex' }}><PiLeaf size={22} style={{ color: '#adadad' }} /><p style={{ color: '#adadad', fontSize: '18px', marginLeft: '15px' }}>Leaf Records</p></Link></li>
-                    <li><Link to="/Agents" style={{ display: 'flex' }}><FaPeopleCarryBox size={22} style={{ color: '#adadad',color: location.pathname === "/Agents" && '#3b5a3b' }} /><p style={{ color: '#adadad',color: location.pathname === "/Agents" && '#3b5a3b', fontSize: '18px', marginLeft: '15px' }}>Agents</p></Link></li>
-                    <li><Link to="/Suppliers" style={{ display: 'flex' }}><MdEmojiPeople size={22} style={{ color: '#adadad' }} /><p style={{ color: '#adadad', fontSize: '18px', marginLeft: '15px' }}>Suppliers</p></Link></li>
+                    <li><Link to="/Dashboard" style={{ display: 'flex' }}><House size={24}  style={{ color: '#adadad' }} /><p style={{ color: '#adadad', fontSize: '16px', marginLeft: '15px' }}>Dashboard</p></Link></li>
+                    <li><Link to="/leaf-records" style={{ display: 'flex' }}><Leaf size={24} style={{ color: '#adadad' }} /><p style={{ color: '#adadad', fontSize: '18px', marginLeft: '15px' }}>Leaf Records</p></Link></li>
+                    <li><Link to="/Agents" style={{ display: 'flex' }}><UsersThree size={24} style={{ color: '#adadad',color: location.pathname === "/Agents" && '#3b5a3b' }} /><p style={{ color: '#adadad',color: location.pathname === "/Agents" && '#3b5a3b', fontSize: '18px', marginLeft: '15px' }}>Agents</p></Link></li>
+                    <li><Link to="/Suppliers" style={{ display: 'flex' }}><Handshake size={24} style={{ color: '#adadad' }} /><p style={{ color: '#adadad', fontSize: '18px', marginLeft: '15px' }}>Suppliers</p></Link></li>
                     <li onClick={toggleManure} style={{ display: 'flex', marginLeft: '8px' }}>
-                        <p style={{ color: '#adadad', fontSize: '20px' }}>Manure</p><IoIosArrowDown size={22} style={{ color: '#adadad', marginLeft: '15px', marginTop: '3px' }} />
+                        <p style={{ color: '#adadad', fontSize: '20px' }}>Manure</p><CaretDown size={24} style={{ color: '#adadad', marginLeft: '15px', marginTop: '3px' }} />
                     </li>
                     <Collapsible open={isManureOpen}>
                         <ul style={{ listStyleType: 'none', paddingLeft: '20px' }}>
-                            <li><Link to="/Suppliers" style={{ display: 'flex' }}><AiOutlineDollarCircle size={22} style={{ color: '#adadad' }} /><p style={{ color: '#adadad', fontSize: '18px', marginLeft: '15px' }}>Manure Sales</p></Link></li>
-                            <li><Link to="/Manure-type-view" style={{ display: 'flex' }}><FaPersonDigging size={22} style={{ color: '#adadad' }} /><p style={{ color: '#adadad', fontSize: '18px', marginLeft: '15px' }}>Manure Types</p></Link></li>
+                            <li><Link to="/Suppliers" style={{ display: 'flex' }}><CurrencyCircleDollar size={24} style={{ color: '#adadad' }} /><p style={{ color: '#adadad', fontSize: '18px', marginLeft: '15px' }}>Manure Sales</p></Link></li>
+                            <li><Link to="/Manure-type-view" style={{ display: 'flex' }}><Shovel size={24} style={{ color: '#adadad' }} /><p style={{ color: '#adadad', fontSize: '18px', marginLeft: '15px' }}>Manure Types</p></Link></li>
                         </ul>
                     </Collapsible>
                     <div style={{ marginTop: '80%' }}>
